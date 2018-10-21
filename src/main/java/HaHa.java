055b4b3ebddbe119c7accd7b4e22e0d8f7022fb7
rick <rick@.com>
2018-10-21 22:03:42 +0800

    kuaijiejian

---
+++ src/main/java/HaHa.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class HaHa {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("666","hahaha");
+        System.out.println(map.get("666"));
+    }
+}
